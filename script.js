99169809a6a24caaf84af3338736864d35024a82
maja669e <maja669e@stud.kea.dk>
2019-05-24 13:34:27 +0200

    i gang med dropdown i afleveringer

--- script.js
+++ script.js
@@ -21,3 +21,12 @@ window.onclick = function (event) {
         }
     }
 }
+
+function myFunction3() {
+    var x = document.getElementById("Demo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
