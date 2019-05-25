eef87f4f0b439c48206fbb3dafd3189f6a1659c7
nitiya <nitiyaihn@gmail.com>
2019-05-25 15:18:27 +0200

    menu1

---
+++ forside.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+
+    document.querySelector("#menu").classList.add("forsvind");  document.querySelector("#forside").classList.remove("forsvind");
+    //Show forside
+    //Hide menu
+    console.log("sidenVises");
+
+    // Queryselector på burger menu
+    // Kalder function show menu
+}
+
+function showMenu(){
+    //Hide forside
+    //Show menu
+
+    // Queryselector på burgermenu/kryds til at komme tilbage til forside
+}
+
